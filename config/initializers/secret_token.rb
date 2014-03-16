0c8b19c457387740c8b8ba5e2a8063cf44a088a3
Jeff Rohalla <jrohalla@uchicago.edu>
2014-03-16 14:59:19 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CardsGame::Application.config.secret_key_base = '924b52654210b48cc77785617632f341a2a52fb6cd67724fc83e95b1549a278ab1c183aad1d697e26236bb18d35da9a16cfee47beba9d31a484c505e1ba8ea97'
